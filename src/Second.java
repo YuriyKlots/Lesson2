e445ad54a88503c0989e21784e102ce65e413656
YuriyKlots <sprklyots@gmail.com>
2019-04-16 19:08:34 +0300

    test

--- src/Second.java
+++ src/Second.java
@@ -1,53 +1,72 @@
 public class Second {
+    // ctrl + alt + l
     public static void main(String[] args) {
-    t3();
+        t3();
     }
-    public static void t1(){
-        int[]  mas = new int[50];
-        int j=1;
-        for (int i = 0;i<mas.length;i++,j+=2)
-        {
+
+    public static void t1() {
+        int[] mas = new int[50];
+        int j = 1;
+        for (int i = 0; i < mas.length; i++, j += 2) {
             mas[i] = j;
         }
 
-        for (int i = 0; i<mas.length;i++){
-            System.out.print(mas[i] + " ");}
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
         System.out.println("\n");
-        for (int i = mas.length-1; i>=0;i--){
-            System.out.print(mas[i] + " ");}
-        System.out.println("\n");
-
-    }
-
-
-    public static void t2(){
-        int[] mas = {2,7,5,9,2,7,5,9};
-
-        for (int i = 0;i<mas.length;i++){
-            if (i%2 == 1) mas[i] = 0;
+        for (int i = mas.length - 1; i >= 0; i--) {
+            System.out.print(mas[i] + " ");
         }
         System.out.println("\n");
 
-        for (int i = 0; i<mas.length;i++){
-            System.out.print(mas[i] + " ");}
+    }
+
+
+    public static void t2() {
+        int[] mas = {2, 7, 5, 9, 2, 7, 5, 9};
+
+        for (int i = 0; i < mas.length; i++) {
+            if (i % 2 == 1) mas[i] = 0;
+        }
+        System.out.println("\n");
+
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
         System.out.println("\n");
 
     }
 
-    public static void t3(){
+    public static void t3() {
         int day = 9;
         String weekday;
 
-
-        switch (day){
-            case 1: weekday = "Понеділок";break;
-            case 2: weekday = "Віторок";break;
-            case 3: weekday = "Середа";break;
-            case 4: weekday = "Четвер";break;
-            case 5: weekday = "П'ятниця";break;
-            case 6: weekday = "Субота";break;
-            case 7: weekday = "Неділя";break;
-            default: weekday = "Некоректні дати";
+// test
+        switch (day) {
+            case 1:
+                weekday = "Понеділок";
+                break;
+            case 2:
+                weekday = "Віторок";
+                break;
+            case 3:
+                weekday = "Середа";
+                break;
+            case 4:
+                weekday = "Четвер";
+                break;
+            case 5:
+                weekday = "П'ятниця";
+                break;
+            case 6:
+                weekday = "Субота";
+                break;
+            case 7:
+                weekday = "Неділя";
+                break;
+            default:
+                weekday = "Некоректні дати";
         }
         System.out.println(weekday);
 
